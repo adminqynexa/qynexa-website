4b7dc00b6e3daeca75b44a286214acf328360bf8
adminqynexa <admin@qynexa.in>
2026-06-17 15:36:10 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,16 @@
 // js/firebase-config.js
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyB5s5U-en6R-Ad6uXhjYMQuC7nXBbh0wPA",
   authDomain: "qynexa-fashion.firebaseapp.com",
   projectId: "qynexa-fashion",
   storageBucket: "qynexa-fashion.firebasestorage.app",
   messagingSenderId: "921391488980",
   appId: "1:921391488980:web:bdfcef3883f363a012c041"
 };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = firebase.firestore();
