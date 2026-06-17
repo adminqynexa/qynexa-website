76e852410a8e86d021b8488511b3ff04e86eb40f
adminqynexa <admin@qynexa.in>
2026-06-17 14:21:15 +0530

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// js/firebase-config.js
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "qynexa-fashion.firebaseapp.com",
+  projectId: "qynexa-fashion",
+  storageBucket: "qynexa-fashion.firebasestorage.app",
+  messagingSenderId: "921391488980",
+  appId: "1:921391488980:web:bdfcef3883f363a012c041"
+};
